0b9c1693cf370b8f8978c0c825c7948f7978ec9f
Michel Albert <michel@albert.lu>
2010-05-16 22:50:02 +0200

    initial commit

---
+++ src/lu/albert/android/bcards/BCardsActivity.java
@@ -0,0 +1,13 @@
+package lu.albert.android.bcards;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BCardsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
